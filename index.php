54687c8b2e9d9b4abc1f5be8a7ae9fd7b8c24f3b
Pattarasawan Sritad <pattarasawan_2542@hotmail.com>
2025-11-12 21:50:08 +0700

    Update page title from 'week2 : Javascript' to 'Piakpound : Resume'

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Piakpound : Resume</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.5.1/dist/chart.umd.min.js"></script>
+</head>
+<body>
+  <h1>Pattarasawan Sritaad</h1>
+</body>
+</html>
